ff9d99d09b81df3e68daceefb036b1999f92c307
sagarciaescobar <sa.garciaescobar@gmail.com>
2022-08-30 11:22:01 -0500

    Add ntfPunks return stament

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React, {useCallback, useEffect, useState} from "react";
+import { useWeb3React } from "@web3-react/core";
+import { useNFTPunks } from "../../hooks/useNFTPunks";
 
 export const Home = () => {
-  return (<p>Hola mundo</p>)
+  const {active} = useWeb3React();
+  const [maxSupply,setMaxSupply] = useState(123);
+  
+  const nftPunks = useNFTPunks();
+
+
+  const getMaxSupply = useCallback(async ()=>{
+    if(nftPunks){
+      const result = await nftPunks.methods.maxSupply().call();
+      setMaxSupply(result);
+    }
+  },[nftPunks])
+
+  useEffect(()=>{
+    getMaxSupply();
+  },[getMaxSupply])
+
+  if(!active) return "conecta tu wallet"
+
+  return (<p>Max Supply: {maxSupply}</p>)
 };
